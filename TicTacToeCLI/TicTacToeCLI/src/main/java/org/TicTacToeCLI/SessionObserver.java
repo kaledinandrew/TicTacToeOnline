9832962aae35e79e48278991a0c2c45f6c06f385
petrov oleg <80612245+necroshine0@users.noreply.github.com>
2022-04-29 01:21:40 +0300

    Delete TicTacToeCLI/TicTacToeCLI/src/main/java/org/TicTacToeCLI directory

--- TicTacToeCLI/TicTacToeCLI/src/main/java/org/TicTacToeCLI/SessionObserver.java
+++
@@ -1,9 +0,0 @@
-package org.TicTacToeCLI;
-
-import java.net.HttpURLConnection;
-
-public interface SessionObserver {
-    public String getUpdate(HttpURLConnection connection);
-    public String postUpdate(HttpURLConnection connection, String jsonInputString);
-    public String putUpdate(HttpURLConnection connection, String jsonInputString);
-}
