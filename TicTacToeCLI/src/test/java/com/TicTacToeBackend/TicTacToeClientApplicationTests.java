f2e875bb2ef5582e611a8d08033aa0d619899a61
Sergei Strizhenok <sstrizhenok@yandex-team.ru>
2022-06-03 02:00:30 +0300

    add test

---
+++ TicTacToeCLI/src/test/java/com/TicTacToeBackend/TicTacToeClientApplicationTests.java
@@ -0,0 +1,12 @@
+package com.TicTacToeBackend;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author sstrizhenok
+ */
+public class TicTacToeClientApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
